afd1d37768b28e60b71f213e7333ca6e5eae727d
Julian So <ch.julian.so@gmail.com>
2026-06-08 17:27:03 -0400

    add play again button

--- battleship-game/src/App.tsx
+++ battleship-game/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 import { Gameboard } from './components/Gameboard';
@@ -8,18 +8,20 @@ import { attack, getComputerMoves } from './game/playerActions';
 import './App.css';
 
 function App() {
-  const [playerBoard] = useState(createPlayerBoard);
-  const [computerBoard] = useState(createComputerBoard);
+  const [play, setPlay] = useState(false);
+  const [playerBoard, setPlayerBoard] = useState(createPlayerBoard);
+  const [computerBoard, setComputerBoard] = useState(createComputerBoard);
   const [turn, setTurn] = useState<'player' | 'computer'>('player');
   const [winner, setWinner] = useState<'player' | 'computer' | null>(null);
 
+  console.log({ winner, play, turn });
+
   function createPlayerBoard() {
     const board = createGameBoard(10);
-
-    board.placeShip(0, 0, 5, 'vertical');
-    board.placeShip(0, 2, 4, 'vertical');
-    board.placeShip(0, 4, 3, 'vertical');
-    board.placeShip(0, 6, 3, 'vertical');
+    // board.placeShip(0, 0, 5, 'vertical');
+    // board.placeShip(0, 2, 4, 'vertical');
+    // board.placeShip(0, 4, 3, 'vertical');
+    // board.placeShip(0, 6, 3, 'vertical');
     board.placeShip(0, 8, 2, 'vertical');
 
     return board;
@@ -28,10 +30,10 @@ function App() {
   function createComputerBoard() {
     const board = createGameBoard(10);
 
-    board.placeShip(0, 0, 5, 'horizontal');
-    board.placeShip(2, 0, 4, 'horizontal');
-    board.placeShip(4, 0, 3, 'horizontal');
-    board.placeShip(6, 0, 3, 'horizontal');
+    // board.placeShip(0, 0, 5, 'horizontal');
+    // board.placeShip(2, 0, 4, 'horizontal');
+    // board.placeShip(4, 0, 3, 'horizontal');
+    // board.placeShip(6, 0, 3, 'horizontal');
     board.placeShip(8, 0, 2, 'horizontal');
 
     return board;
@@ -46,6 +48,8 @@ function App() {
 
     if (gameOver) {
       setWinner('player');
+      setPlay(false);
+      return;
     }
 
     setTurn('computer');
@@ -57,7 +61,10 @@ function App() {
 
     if (gameOver) {
       setWinner('computer');
+      setPlay(false);
+      return;
     }
+
     setTurn('player');
   }
 
@@ -71,16 +78,36 @@ function App() {
     }
   }, [turn]);
 
+  function startNewGame() {
+    const newPlayerBoard = createPlayerBoard();
+    const newComputerBoard = createComputerBoard();
+
+    setWinner(null);
+    setPlay(true);
+    setTurn('player');
+    setPlayerBoard(newPlayerBoard);
+    setComputerBoard(newComputerBoard);
+  }
+
+  function handlePlay() {
+    if (winner) {
+      startNewGame();
+    } else if (!play) {
+      setPlay(true);
+    }
+  }
+
   return (
     <>
       <h1>{winner === null ? '' : winner === 'player' ? 'Player Wins!' : 'Computer Wins!'}</h1>
       <div className='gameboard_playarea'>
         <div>
-          <Gameboard gameboard={playerBoard} handleAttack={handleComputerAttack} />
+          <Gameboard gameboard={playerBoard} play={play} />
         </div>
-        <div>
-          <Gameboard gameboard={computerBoard} handleAttack={handlePlayerAttack} />
+        <div className={!play ? 'opacity-50' : ''}>
+          <Gameboard gameboard={computerBoard} handleAttack={handlePlayerAttack} play={play} />
         </div>
+        {!play ? <button onClick={handlePlay}>{winner ? 'Play Again' : 'play'}</button> : ''}
       </div>
     </>
   );
--- battleship-game/src/components/Gameboard.tsx
+++ battleship-game/src/components/Gameboard.tsx
@@ -4,18 +4,21 @@ type Gameboard = ReturnType<typeof createGameBoard>;
 type GameboardProps = {
   gameboard: Gameboard;
   handleAttack?: (row: number, col: number) => void;
+  play: boolean;
 };
 
-function Gameboard({ gameboard, handleAttack }: GameboardProps) {
+function Gameboard({ gameboard, handleAttack, play }: GameboardProps) {
+  const isClickable = play && handleAttack;
+
   return (
     <>
-      <div className='gameboard_grid'>
+      <div className={'gameboard_grid'}>
         {gameboard.grid.map((row, rowIndex) =>
           row.map((cell, colIndex) => (
             <button
               key={`${rowIndex}-${colIndex}`}
               onClick={() => handleAttack?.(rowIndex, colIndex)}
-              disabled={!handleAttack}
+              disabled={!isClickable}
               className={`gameboard_cell ${cell === null ? 'gameboard_cell-water' : 'gameboard_cell-ship'}
                 ${gameboard.isMiss(rowIndex, colIndex) ? 'gameboard_cell-miss' : ''}
                 ${gameboard.isHit(rowIndex, colIndex) ? 'gameboard_cell-hit' : ''}
