f5584d81151774384a7a56328f0cecbe68cb182f
Julian So <ch.julian.so@gmail.com>
2026-06-08 22:58:19 -0400

    add row and col labels and css

--- battleship-game/src/components/Gameboard.tsx
+++ battleship-game/src/components/Gameboard.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import createGameBoard from '../game/createGameboard';
 
 type Gameboard = ReturnType<typeof createGameBoard>;
@@ -9,27 +11,35 @@ type GameboardProps = {
 
 function Gameboard({ gameboard, handleAttack, play }: GameboardProps) {
   const isClickable = play && handleAttack;
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
   return (
-    <>
-      <div className={'gameboard_grid'}>
-        {gameboard.grid.map((row, rowIndex) =>
-          row.map((cell, colIndex) => (
+    <div className='gameboard_wrapper'>
+      <div />
+      {letters.map((letter) => (
+        <div className='gameboard_col-label' key={letter}>
+          {letter}
+        </div>
+      ))}
+      {gameboard.grid.map((row, rowIndex) => (
+        <React.Fragment key={rowIndex}>
+          <div className='gameboard_row-label'>{rowIndex + 1}</div>
+          {row.map((cell, colIndex) => (
             <button
               key={`${rowIndex}-${colIndex}`}
               onClick={() => handleAttack?.(rowIndex, colIndex)}
               disabled={!isClickable}
-              className={`gameboard_cell ${cell === null ? 'gameboard_cell-water' : 'gameboard_cell-ship'}
+              className={`gameboard_cell ${handleAttack ? 'gameboard_cell-computer' : ''} ${cell === null ? 'gameboard_cell-water' : 'gameboard_cell-ship'}
                 ${gameboard.isMiss(rowIndex, colIndex) ? 'gameboard_cell-miss' : ''}
                 ${gameboard.isHit(rowIndex, colIndex) ? 'gameboard_cell-hit' : ''}
               `}
             >
               {cell === null ? '' : ''}
             </button>
-          )),
-        )}
-      </div>
-    </>
+          ))}
+        </React.Fragment>
+      ))}
+    </div>
   );
 }
 
